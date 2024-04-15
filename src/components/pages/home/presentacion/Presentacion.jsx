9e78eadb85b13451b65ca6793813feb2a71eb8d4
Facundo Caamaño <facundoadriancaamano@outlook.com>
2024-04-15 17:33:27 -0300

    alt imgs, rxjs, angular material

--- src/components/pages/home/presentacion/Presentacion.jsx
+++ src/components/pages/home/presentacion/Presentacion.jsx
@@ -7,7 +7,7 @@ export const Presentacion =({isDarkMode, setIsDarkMode})=>{
                 <div className="containerPresentacion">
                     <div className="intro">
                         <h1 className={isDarkMode ? "darkModeH1" : ""}>Hola, soy <span className="highlight">Facundo Caamaño</span></h1>
-                        <p className={isDarkMode ? "darkModeP" : "ligthModeP"}>Soy un apasionado desarrollador frontend con experiencia en la creación de experiencias web increíbles. Me encanta diseñar y desarrollar interfaces de usuario atractivas y funcionales utilizando programación orientada a objetos y programación reactiva.</p>
+                        <p className={isDarkMode ? "darkModeP" : "ligthModeP"}>Soy un apasionado desarrollador web con experiencia en la creación de experiencias web increíbles. Me encanta diseñar y desarrollar interfaces de usuario atractivas y funcionales utilizando programación orientada a objetos y programación reactiva.</p>
                     </div>
                     <div className="profile-image">
                         <img src={foto} alt="Tu Foto" />
--- src/components/pages/home/skills/Skills.jsx
+++ src/components/pages/home/skills/Skills.jsx
@@ -17,7 +17,7 @@ export const Skills = () => {
                             data-bs-placement="top"
                             data-bs-custom-class="custom-tooltip"
                             data-bs-title="HTML" 
-                            className="skill--img" src="https://www.svgrepo.com/show/452228/html-5.svg" alt="" />
+                            className="skill--img" src="https://www.svgrepo.com/show/452228/html-5.svg" alt="HTML" />
                     </div>
                     <div className="col">
                         <img 
@@ -25,7 +25,7 @@ export const Skills = () => {
                         data-bs-placement="top"
                         data-bs-custom-class="custom-tooltip"
                         data-bs-title="ReactJs" 
-                        className="skill--img" src="https://www.svgrepo.com/show/374032/reactjs.svg" alt="" />
+                        className="skill--img" src="https://www.svgrepo.com/show/374032/reactjs.svg" alt="ReactJs" />
                     </div>
 
                     <div className="col">
@@ -34,7 +34,7 @@ export const Skills = () => {
                         data-bs-placement="top"
                         data-bs-custom-class="custom-tooltip"
                         data-bs-title="Angular" 
-                        className="skill--img" src="https://www.svgrepo.com/show/452156/angular.svg" alt="" />
+                        className="skill--img" src="https://www.svgrepo.com/show/452156/angular.svg" alt="Angular" />
                     </div>
                 </div>
                 <div className="row">
@@ -44,7 +44,7 @@ export const Skills = () => {
                         data-bs-placement="top"
                         data-bs-custom-class="custom-tooltip"
                         data-bs-title="CSS" 
-                        className="skill--img" src="https://www.svgrepo.com/show/452185/css-3.svg" alt="" />
+                        className="skill--img" src="https://www.svgrepo.com/show/452185/css-3.svg" alt="CSS" />
                     </div>
                     <div className="col">
                         <img 
@@ -52,7 +52,7 @@ export const Skills = () => {
                         data-bs-placement="top"
                         data-bs-custom-class="custom-tooltip"
                         data-bs-title="SCSS" 
-                        className="skill--img" src="https://www.svgrepo.com/show/374067/scss2.svg" alt="" />
+                        className="skill--img" src="https://www.svgrepo.com/show/374067/scss2.svg" alt="SCSS" />
                     </div>
                     <div className="col">
                         <img 
@@ -60,7 +60,7 @@ export const Skills = () => {
                         data-bs-placement="top"
                         data-bs-custom-class="custom-tooltip"
                         data-bs-title="TypeScript" 
-                        className="skill--img" src="https://www.svgrepo.com/show/374146/typescript-official.svg" alt="" />
+                        className="skill--img" src="https://www.svgrepo.com/show/374146/typescript-official.svg" alt="TypeScript" />
                     </div>
                 </div>
                 <div className="row">
@@ -70,7 +70,7 @@ export const Skills = () => {
                         data-bs-placement="top"
                         data-bs-custom-class="custom-tooltip"
                         data-bs-title="Javascript" 
-                        className="skill--img" src="https://www.svgrepo.com/show/353925/javascript.svg" alt="" />
+                        className="skill--img" src="https://www.svgrepo.com/show/353925/javascript.svg" alt="Javascript" />
                     </div>
                     <div className="col">
                         <img 
@@ -78,7 +78,7 @@ export const Skills = () => {
                         data-bs-placement="top"
                         data-bs-custom-class="custom-tooltip"
                         data-bs-title="Bootstrap" 
-                        className="skill--img" src="https://www.svgrepo.com/show/353498/bootstrap.svg" alt="" />
+                        className="skill--img" src="https://www.svgrepo.com/show/353498/bootstrap.svg" alt="Bootstrap" />
                     </div>
                     <div className="col">
                         <img 
@@ -86,7 +86,7 @@ export const Skills = () => {
                         data-bs-placement="top"
                         data-bs-custom-class="custom-tooltip"
                         data-bs-title="Angular Material" 
-                        className="skill--img" src="https://material.angular.io/assets/img/angular-material-logo.svg" alt="" />
+                        className="skill--img" src="https://angular.io/generated/images/marketing/concept-icons/material.png" alt="angular material" />
                     </div>
                 </div>
                 <div className="row">
@@ -104,15 +104,15 @@ export const Skills = () => {
                         data-bs-placement="top"
                         data-bs-custom-class="custom-tooltip"
                         data-bs-title="GIT" 
-                        className="skill--img" src=" https://www.svgrepo.com/show/452210/git.svg" alt="" />
+                        className="skill--img" src=" https://www.svgrepo.com/show/452210/git.svg" alt="GIT" />
                     </div>
                     <div className="col">
                         <img 
                         data-bs-toggle="tooltip" 
                         data-bs-placement="top"
                         data-bs-custom-class="custom-tooltip"
-                        data-bs-title="NGRX"   
-                        className="skill--img" src="https://ngrx.io/assets/images/badge.svg" alt="" />
+                        data-bs-title="RxJs"   
+                        className="skill--img" src="https://cdn.worldvectorlogo.com/logos/rxjs-1.svg" alt="rxjs" />
                     </div>
                 </div>
             </div>
